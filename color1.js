b4325a618bb5f4645c583fa66603b762ea21f869
Akshaythupakula <akshaygoud509@gmail.com>
2024-04-22 12:54:16 +0530

    modified

--- color1.js
+++ color1.js
@@ -1,140 +1,145 @@
 
-  const container = document.querySelector('.container');
-  const centerX = container.offsetWidth / 2;
-  const centerY = container.offsetHeight / 2;
-  const centerRadius = 100; // Adjust as needed
-  const array = ['#ed710c', '#b4e112', '#0cd16e', '#6e17d9', '#062f4a', '#f705f3'];
-  let coloredice = ''
-  for (let i = 0; i < 1; i++) {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    const randomElement = array[randomIndex];
-    const angle = (i / 1) * Math.PI * 2;
-    const x = centerX + 0 * Math.cos(angle);
-    const y = centerY + 0 * Math.sin(angle);
+const container = document.querySelector('.container');
+const centerX = container.offsetWidth / 2;
+const centerY = container.offsetHeight / 2;
+const centerRadius = 100; // Adjust as needed
+const array = ['#ed710c', '#b4e112', '#0cd16e', '#6e17d9', '#062f4a', '#f705f3'];
+let coloredice = ''
+for (let i = 0; i < 1; i++) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const randomElement = array[randomIndex];
+  const angle = (i / 1) * Math.PI * 2;
+  const x = centerX + 0 * Math.cos(angle);
+  const y = centerY + 0 * Math.sin(angle);
 
-    const circle = document.createElement('div');
-    circle.className = 'circle';
-    circle.style.left = `${x}px`;
-    circle.style.top = `${y}px`;
-    circle.addEventListener('click', () => {
-      circle.style.backgroundColor = randomElement;
+  const circle = document.createElement('div');
+  circle.className = 'circle';
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
+  circle.addEventListener('click', () => {
+    circle.style.backgroundColor = randomElement;
 
-      if (randomElement == coloredice) {
-        circle.style.backgroundColor = '#ffff';
-        circle.style.display = 'none';
-        console.log(coloredice, 'color')
+    if (randomElement == coloredice) {
+      circle.style.backgroundColor = '#ffff';
+      circle.style.display = 'none';
+      console.log(coloredice, 'color')
 
-      } else {
-        circle.addEventListener('mouseleave', () => {
-          circle.style.backgroundColor = '#3498db';
-          console.log(coloredice, 'color')
-        });
-
-      }
-    });
-    container.appendChild(circle);
-  }
-
-  for (let i = 0; i < 7; i++) {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    const randomElement = array[randomIndex];
-    const angle = (i / 7) * Math.PI * 2;
-    const x = centerX + centerRadius * Math.cos(angle);
-    const y = centerY + centerRadius * Math.sin(angle);
-
-    const circle = document.createElement('div');
-    circle.className = 'circle';
-    circle.style.left = `${x}px`;
-    circle.style.top = `${y}px`;
-    circle.addEventListener('click', () => {
-      circle.style.backgroundColor = randomElement;
-
-      if (randomElement == coloredice) {
-        circle.style.backgroundColor = '#ffff';
-        circle.style.display = 'none';
-        console.log(coloredice, 'color')
-
-      } else {
-        circle.addEventListener('mouseleave', () => {
-          circle.style.backgroundColor = '#3498db';
-          console.log(coloredice, 'color')
-        });
-
-      }
-    });
-
-
-    container.appendChild(circle);
-  }
-
-  const radius = Math.min(centerX, centerY) - 50; // Adjust for circle radius and spacing
-
-  for (let i = 0; i < 14; i++) {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    const randomElement = array[randomIndex];
-    const angle = (i / 14) * Math.PI * 2;
-    const x = centerX + radius * Math.cos(angle);
-    const y = centerY + radius * Math.sin(angle);
-
-    const circle = document.createElement('div');
-    circle.className = 'circle';
-    circle.style.left = `${x}px`;
-    circle.style.top = `${y}px`;
-
-    circle.addEventListener('click', () => {
-      circle.style.backgroundColor = randomElement;
-
-      if (randomElement == coloredice) {
-        circle.style.backgroundColor = '#ffff';
-        circle.style.display = 'none';
-        console.log(coloredice, 'color')
-
-      } else {
-        circle.addEventListener('mouseleave', () => {
-          circle.style.backgroundColor = '#3498db';
-          console.log(coloredice, 'color')
-        });
-
-      }
-    });
-
-
-    container.appendChild(circle);
-  }
-
-  function rollDice() {
-    const dice = [...document.querySelectorAll(".die-list")];
-    dice.forEach(die => {
-      toggleClasses(die);
-      die.dataset.roll = getRandomNumber(1, 6);
-
-    });
-  }
-
-  function toggleClasses(die) {
-    die.classList.toggle("odd-roll");
-    die.classList.toggle("even-roll");
-  }
-
-  function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    let a = Math.floor(Math.random() * (max - min + 1)) + min
-    if (a == 1) {
-      coloredice = '#ed710c'
-    } else if (a == 2) {
-      coloredice = '#b4e112'
-    } else if (a == 3) {
-      coloredice = '#0cd16e'
-    } else if (a == 4) {
-      coloredice = '#6e17d9'
-    } else if (a == 5) {
-      coloredice = '#062f4a'
     } else {
-      coloredice = '#f705f3'
-    }
-    return a;
-  }
+      circle.addEventListener('mouseleave', () => {
+        circle.style.backgroundColor = '#3498db';
+        console.log(coloredice, 'color')
+      });
 
-  document.getElementById("roll-button").addEventListener("click", rollDice);
+    }
+  });
+  container.appendChild(circle);
+}
+
+for (let i = 0; i < 7; i++) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const randomElement = array[randomIndex];
+  const angle = (i / 7) * Math.PI * 2;
+  const x = centerX + centerRadius * Math.cos(angle);
+  const y = centerY + centerRadius * Math.sin(angle);
+
+  const circle = document.createElement('div');
+  circle.className = 'circle';
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
+  circle.addEventListener('click', () => {
+    circle.style.backgroundColor = randomElement;
+
+    if (randomElement == coloredice) {
+      circle.style.backgroundColor = '#ffff';
+      circle.style.display = 'none';
+      console.log(coloredice, 'color')
+
+    } else {
+      circle.addEventListener('mouseleave', () => {
+        circle.style.backgroundColor = '#3498db';
+        console.log(coloredice, 'color')
+      });
+
+    }
+  });
+
+
+  container.appendChild(circle);
+}
+
+const radius = Math.min(centerX, centerY) - 50; // Adjust for circle radius and spacing
+
+for (let i = 0; i < 14; i++) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  const randomElement = array[randomIndex];
+  const angle = (i / 14) * Math.PI * 2;
+  const x = centerX + radius * Math.cos(angle);
+  const y = centerY + radius * Math.sin(angle);
+
+  const circle = document.createElement('div');
+  circle.className = 'circle';
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
+
+  circle.addEventListener('click', () => {
+    circle.style.backgroundColor = randomElement;
+
+
+    if (randomElement == coloredice) {
+      circle.style.backgroundColor = coloredice;
+      circle.style.pointerEvents= 'none';
+      circle.addEventListener('mouseleave', () => {
+        circle.style.backgroundColor = coloredice;
+        console.log(coloredice, 'color')
+      });
+      console.log(coloredice, 'color')
+
+    } else {
+      circle.addEventListener('mouseleave', () => {
+        circle.style.backgroundColor = '#3498db';
+        console.log(coloredice, 'color')
+      });
+
+    }
+  });
+
+
+  container.appendChild(circle);
+}
+
+function rollDice() {
+  const dice = [...document.querySelectorAll(".die-list")];
+  dice.forEach(die => {
+    toggleClasses(die);
+    die.dataset.roll = getRandomNumber(1, 6);
+
+  });
+}
+
+function toggleClasses(die) {
+  die.classList.toggle("odd-roll");
+  die.classList.toggle("even-roll");
+}
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let a = Math.floor(Math.random() * (max - min + 1)) + min
+  if (a == 1) {
+    coloredice = '#ed710c'
+  } else if (a == 2) {
+    coloredice = '#b4e112'
+  } else if (a == 3) {
+    coloredice = '#0cd16e'
+  } else if (a == 4) {
+    coloredice = '#6e17d9'
+  } else if (a == 5) {
+    coloredice = '#062f4a'
+  } else {
+    coloredice = '#f705f3'
+  }
+  return a;
+}
+
+document.getElementById("roll-button").addEventListener("click", rollDice);
 
